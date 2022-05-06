71d904e91bce152819d532305f2f17da19df4b0c
MinZombie <78908490+MinZombie@users.noreply.github.com>
2022-05-06 21:10:16 +0900

    FavoriteListViewController 구현

--- iLikeMovie/View/FavoriteListViewController.swift
+++ iLikeMovie/View/FavoriteListViewController.swift
@@ -8,9 +8,18 @@
 import UIKit
 
 class FavoriteListViewController: UIViewController {
+    // MARK: - Properties
+    
+    private let tableView: UITableView = {
+       let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(MovieListTableViewCell.self, forCellReuseIdentifier: MovieListTableViewCell.identifier)
+        return tableView
+    }()
     
     let viewModel: MovieListViewModel
     
+    // MARK: - Init
     init(viewModel: MovieListViewModel) {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
@@ -20,10 +29,52 @@ class FavoriteListViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .red
+        view.backgroundColor = .systemBackground
         title = "즐겨찾기"
+        
+        setUpTableView()
+        
+        bind(viewModel: viewModel)
+    }
+    
+    // MARK: - Private
+    private func bind(viewModel: MovieListViewModel) {
+        
+    }
+    
+    private func setUpTableView() {
+        view.addSubview(tableView)
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 0),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 0),
+        ])
+    }
+}
+
+// MARK: - UITableViewDataSource, UITableViewDelegate
+extension FavoriteListViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.movies.value.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: MovieListTableViewCell.identifier, for: indexPath) as? MovieListTableViewCell else { return UITableViewCell() }
+        
+        cell.configure(with: viewModel.movies.value[indexPath.row])
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return MovieListTableViewCell.height
     }
 }
