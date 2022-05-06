80362d885b9edc75669be0e6ee9af63349d8f017
MinZombie <78908490+MinZombie@users.noreply.github.com>
2022-05-06 16:24:51 +0900

    first commit

---
+++ iLikeMovie/View/MovieListTableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iLikeMovie
+//
+//  Created by 민선기 on 2022/05/06.
+//
+
+import UIKit
+
+class MovieListTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
